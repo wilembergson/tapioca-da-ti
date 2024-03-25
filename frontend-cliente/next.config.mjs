6c55dfdf30a20e4af4efaf3b9c323be60bccc251
wilembergson <wilembergson@yahoo.com>
2024-03-24 21:44:03 -0300

    fix: ajustando cors

--- frontend-cliente/next.config.mjs
+++ frontend-cliente/next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    async headers() {
+        return [
+          {
+            // Defina os cabeçalhos que você deseja permitir
+            source: '/',
+            headers: [
+              {
+                key: 'Access-Control-Allow-Origin',
+                value: '*', // Permitir solicitações de todas as origens
+              },
+              {
+                key: 'Access-Control-Allow-Headers',
+                value: 'Origin, X-Requested-With, Content-Type, Accept',
+              },
+              {
+                key: 'Access-Control-Allow-Methods',
+                value: 'GET, POST, PUT, DELETE, OPTIONS',
+              },
+            ],
+          },
+        ];
+      },
 };
 
 export default nextConfig;
